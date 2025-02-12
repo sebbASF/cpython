7350b35febfc2613c158940679e19fe6476f9b31
Sebb <sebb@apache.org>
2025-02-12 23:56:54 +0000

    Better test message

--- Lib/test/test_email/test_versions.py
+++ Lib/test/test_email/test_versions.py
@@ -2,17 +2,17 @@
 
 import sys
 import email
-from test.test_email import openfile
+# from test.test_email import openfile
 import hashlib
 
 def msgobj(filename):
-    with openfile(filename, encoding="utf-8") as fp:
+    with open(filename, encoding="utf-8") as fp:
         data = fp.read()
     msg = email.message_from_string(data)
     return msg, data
 
 def main():
-    msg, data = msgobj('msg_16.txt')
+    msg, data = msgobj('Lib/test/test_email/data/msg_50.txt')
     asbytes = msg.as_bytes()
     print(sys.version)
     print(f"data:  len={len(data)} hash={hashlib.md5(data.encode()).hexdigest()}")
