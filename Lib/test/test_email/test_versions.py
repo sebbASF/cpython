2d69c07a4a8984d5a6e5620121a04399685bca9f
Sebb <sebb@apache.org>
2025-02-12 23:10:55 +0000

    Try to find break

---
+++ Lib/test/test_email/test_versions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+import email
+from test.test_email import openfile
+import hashlib
+
+def msgobj(filename):
+    with openfile(filename, encoding="utf-8") as fp:
+        data = fp.read()
+    msg = email.message_from_string(data)
+    return msg, data
+
+def main():
+    msg, data = msgobj('msg_16.txt')
+    asbytes = msg.as_bytes()
+    print(sys.version)
+    print(f"data:  len={len(data)} hash={hashlib.md5(data.encode()).hexdigest()}")
+    print(f"bytes: len={len(asbytes)} hash={hashlib.md5(asbytes).hexdigest()} ")
+    
+if __name__ == '__main__':
+    main()
