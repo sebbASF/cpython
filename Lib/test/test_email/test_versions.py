4c576f47e8b18bfe9961e4e5243f5025d663004a
Sebb <sebb@apache.org>
2025-02-13 00:44:20 +0000

    No fold

--- Lib/test/test_email/test_versions.py
+++ Lib/test/test_email/test_versions.py
@@ -3,13 +3,15 @@
 # Show that msg.as_bytes() is not consistent across Python versions
 
 import email
+import email.policy
 import hashlib
 
 def testmsg(filename):
     print(filename)
+    policy = email.policy.default.clone(refold_source='none')
     with open(f"Lib/test/test_email/data/{filename}", encoding="utf-8") as fp:
         data = fp.read()
-    msg = email.message_from_string(data)
+    msg = email.message_from_string(data,policy=policy)
     asbytes = msg.as_bytes()
     print(f"data:  len={len(data)} hash={hashlib.md5(data.encode()).hexdigest()}")
     print(f"bytes: len={len(asbytes)} hash={hashlib.md5(asbytes).hexdigest()} ")
