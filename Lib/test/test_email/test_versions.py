f71fcc4079556e244c806cc08a246aa1b5e0a0ad
Sebb <sebb@apache.org>
2025-02-13 00:05:27 +0000

    New mesg

--- Lib/test/test_email/test_versions.py
+++ Lib/test/test_email/test_versions.py
@@ -2,21 +2,19 @@
 
 import sys
 import email
-# from test.test_email import openfile
 import hashlib
 
-def msgobj(filename):
-    with open(filename, encoding="utf-8") as fp:
+def testmsg(filename):
+    with open(f"Lib/test/test_email/data/{filename}", encoding="utf-8") as fp:
         data = fp.read()
     msg = email.message_from_string(data)
-    return msg, data
-
-def main():
-    msg, data = msgobj('Lib/test/test_email/data/msg_50.txt')
     asbytes = msg.as_bytes()
-    print(sys.version)
     print(f"data:  len={len(data)} hash={hashlib.md5(data.encode()).hexdigest()}")
     print(f"bytes: len={len(asbytes)} hash={hashlib.md5(asbytes).hexdigest()} ")
+
+def main():
+    testmsg('msg_50.txt')
+    testmsg('msg_51.txt')
     
 if __name__ == '__main__':
     main()
